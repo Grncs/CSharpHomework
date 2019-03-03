1abef838b52abdd7a4ca1ce4d07427b771a38b2e
Grncs <2907115224@qq.com>
2019-03-04 00:21:19 +0800

    Add files via upload
    homework 2: page 78-6、7、9

---
+++ homework2/p78-6.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+
+   class Class1
+   {
+    public static void Main(string[] args)
+    {
+        Boolean flag;
+        int n = 0;
+        Console.WriteLine("\n请输入一个数字，程序将输出它的所有素数因子\n");
+        string s = Console.ReadLine();
+        int input = int.Parse(s);
+        Console.WriteLine();
+        for(int i = 2; i <= input; i++)
+        {
+            flag = true;
+            if (input % i != 0)
+                continue;
+            else
+                for(int j = 2; j<i; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        flag = false;
+                        break;
+                    }
+                }
+            if (flag == false)
+                continue;
+            else
+            {
+                if (n < 10)
+                {
+                    Console.Write(i + " ");
+                    n++;
+                }
+                else
+                {
+                    Console.WriteLine();
+                    Console.Write(i + " ");
+                    n = 1;
+                }
+            }
+        
+        }
+        Console.ReadKey();
+       }
+    }
