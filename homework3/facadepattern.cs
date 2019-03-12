3999606f27950754750885510422e29053364c6d
ncs <2907115224@qq.com>
2019-03-12 17:01:43 +0800

    homework3

---
+++ homework3/facadepattern.cs
@@ -0,0 +1,67 @@
+﻿/* 外观设计模式 */
+using System;
+using System.Text;
+using System.IO;
+public class Reader
+{
+    public string Read()
+    {
+        string s = ""; 
+        Console.Write("请输入一段字符\n");
+        try
+        {
+            s = Console.ReadLine();
+        }
+        catch (Exception e)
+        {
+            Console.Write(e.Message);
+        }
+        return s;
+    }
+}
+
+public class Encrypter
+{
+    public string Encrypt(string s)
+    {
+        string news = "";
+        char[] vs = s.ToCharArray();
+        foreach(char c in vs)
+        {
+            news += (c % 9).ToString();
+        }
+        return news;
+    }
+}
+
+public class Writer
+{
+    public void Write(string s)
+    {
+        Console.Write("加密结果为" + s + "\n");
+
+    }
+}
+
+public class EncryptFacade
+{
+    private Reader s1 = new Reader();
+    private Encrypter s2 = new Encrypter();
+    private Writer s3 = new Writer();
+    public void Work()
+    {
+        string text;
+        text = s1.Read();
+        text = s2.Encrypt(text);
+        s3.Write(text);
+    }
+    public static void Main()
+    {
+        EncryptFacade EF = new EncryptFacade();
+        EF.Work();
+        return;
+    }
+
+}
+
+
---
+++ homework3/factorymethodpattern.cs
@@ -0,0 +1,70 @@
+﻿
+/*工厂方法模式*/
+
+using System;
+
+public interface Logger
+{
+    void writelog();
+}
+
+public interface Factory
+{
+    Logger createlogger();
+}
+
+public class Databaselogger : Logger
+{
+    public void writelog()
+    {
+        Console.Write("数据库日志\n");
+    }
+    public Databaselogger()
+    {
+        Console.Write("生成数据库日志\n");
+    }
+}
+
+public class Filelogger : Logger
+{
+    public void writelog()
+    {
+        Console.Write("文件日志\n");
+    }
+    public Filelogger()
+    {
+        Console.Write("生成文件日志\n");
+    }
+}
+
+public class Filefactory : Factory
+{
+    public Logger createlogger()
+    {
+        Logger logger = new Databaselogger();
+        return logger;
+    }
+}
+
+public class Datafactory : Factory
+{
+    public Logger createlogger()
+    {
+        Logger logger = new Filelogger();
+        return logger;
+    }
+}
+
+public class Client
+{
+    public static void Main()
+    {
+        Factory loggerfactory = new Datafactory();
+        Logger datalogger = loggerfactory.createlogger();
+        datalogger.writelog();
+        Factory loggerfactory2 = new Filefactory();
+        Logger filelogger = loggerfactory2.createlogger();
+        filelogger.writelog();
+        return;
+    }
+}
